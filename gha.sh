4fe1a7725595c59963510624bfa162d858786b0a
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-13 22:12:49 -0700

    update gha.sh

--- gha.sh
+++ gha.sh
@@ -1,16 +1,12 @@
 #! usr/bin/env bash
-read -p "Login?" -n 1 -r
+gh auth login -s delete_repo 
+read -p "Extend?" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	gh auth login -s delete_repo 
-	read -p "Extend?" -n 1 -r
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		sudo apt-get install fzf jq -y
-		gh extension install yusukebe/gh-markdown-preview
-		gh extension install korosuke613/gh-user-stars
-		gh extension install gennaro-tedesco/gh-s
-		gh alias set stars gh-user-stars
-		gh alias set p markdown-preview
-	fi
+	sudo apt-get install fzf jq -y
+	gh extension install yusukebe/gh-markdown-preview
+	gh extension install korosuke613/gh-user-stars
+	gh extension install gennaro-tedesco/gh-s
+	gh alias set stars gh-user-stars
+	gh alias set p markdown-preview
 fi
