ac7bcfbf26bbb200e8a28503a26888a51f4843be
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-17 08:17:40 -0700

    update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -1,2 +1,10 @@
 #! usr/bin/env bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+cd ~
+git clone https://github.com/nvm-sh/nvm .nvm
+cd ~/.nvm
+git checkout v0.39.3
+. ./nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
