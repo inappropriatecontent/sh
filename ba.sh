d7888bda9cb7636993f971c6608da5dc7cd2c2d1
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-17 14:33:49 -0700

    update ba.sh

--- ba.sh
+++ ba.sh
@@ -3,3 +3,12 @@ sudo apt-get update
 sudo apt-get install -y fdfind
 sudo mv /usr/bin/fdfind /usr/bin/fd
 sudo apt-get upgrade -y
+read -p "Do you want to continue? (yes/no) " answer
+if [ "$answer" = "yes" ]; then
+  echo "OK, let's go on"
+elif [ "$answer" = "no" ]; then
+  echo "Bye"
+  exit
+else
+  echo "Invalid input"
+fi
