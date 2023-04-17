c14f2114935c2d6e99d2c507deb4c3de3d7b0359
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-17 14:50:33 -0700

    update ba.sh

--- ba.sh
+++ ba.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 mod() {
-read -p "Get $($1)? No to quit." -n 1 -ei Y resp
-if ["$resp" = [Yy]*]; then
+	read -p "Get $($1)? No to quit." -n 1 -ei Y resp
+	if ["$resp" = [Yy]*]; then
 	bash "~/$($2).sh"
-elif ["$resp" = "no"]; then
-	exit
-else
-	sudo apt-get upgrade -y
-fi	
+	elif ["$resp" = "no"]; then
+	exit -1
+	else
+		sudo apt-get upgrade -y
+	fi
 }
 
 cd ~
 sudo apt-get update
 mod 'FD-Find' 'fd'
-
