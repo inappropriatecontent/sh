132d3adc6c984990acb1fc4d4fe59b35ad8cbe2c
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-17 14:01:42 -0700

    update xplr.sh

--- xplr.sh
+++ xplr.sh
@@ -1,5 +1,5 @@
 #! usr/bin/env bash
-platform="linux"  # or "macos" / "linux-musl"
+local platform = "linux"  # or "macos" / "linux-musl"
 wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-$platform.tar.gz
 tar xzvf xplr-$platform.tar.gz
 sudo mv xplr /usr/local/bin/
