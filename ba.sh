5ad337b49e33aec2226ec5d9c243b2fc21466442
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-17 20:52:45 -0700

    update ba.sh

--- ba.sh
+++ ba.sh
@@ -2,7 +2,8 @@
 cd sh
 chmod a+x fd.sh gh.sh gha.sh nvm.sh xplr.sh
 mod() {
-	2 = echo "$1" | sed 's/.sh$//'
+	local fn = $1,
+	st= "$1" | sed 's/.sh$//'
 	read -p "Install?" -ei "$2" reply
 	if ["$reply" = "$2"]; then
 	echo "./$1"
