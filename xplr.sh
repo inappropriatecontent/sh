38a0debe5b402952d18715c9e5a25192de6fe647
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-18 00:58:26 -0700

    update xplr.sh

--- xplr.sh
+++ xplr.sh
@@ -3,10 +3,10 @@ wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-linux.tar
 tar xzvf xplr-linux.tar.gz
 sudo mv xplr /usr/local/bin/
 rm xplr-linux.tar.gz
-alias xd='cd "$(xplr --print-pwd-as-result)"'
-if [ type -p gh >/dev/null ]
+alias xd = 'cd "$(xplr --print-pwd-as-result)"'
+if [type -p gh >/dev/null]
 then
-	gh extension install sayanarijit/gh-xplr 
-	gh alias set x xplr
-	alias ghx='read -r -p "Clone Search?" | gh xplr "$(gh s $1)i"'
+gh extension install sayanarijit/gh-xplr
+gh alias set x xplr
+alias ghx = 'read -r -p "Clone Search?" | gh xplr "$(gh s $1)i"'
 fi
