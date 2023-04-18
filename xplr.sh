a0dd200b098f7e91952cb467c6f0a221e9bf81e8
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-17 21:42:29 -0700

    Update xplr.sh

--- xplr.sh
+++ xplr.sh
@@ -2,3 +2,5 @@
 wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-linux.tar.gz
 tar xzvf xplr-linux.tar.gz
 sudo mv xplr /usr/local/bin/
+rm xplr-linux.tar.gz
+alias xd='cd "$(xplr --print-pwd-as-result)"'
