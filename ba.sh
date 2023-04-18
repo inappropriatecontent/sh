7ea7a025fb2b4675b9eb8541ceb5513962ee0d43
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-18 00:51:34 -0700

    update ba.sh

--- ba.sh
+++ ba.sh
@@ -3,22 +3,22 @@ cd ~./sh
 apt() {
 	sudo apt-get update -y >/dev/null
 	sudo apt-get upgrade -y >/dev/null
-	if [[ $1 != '' ]]
-		sudo apt-get install -y $1
+	if [[$1 != '']]
+	sudo apt-get install -y $1
 	fi
 }
 mod() {
 	read -n 1 -r -p "Install $1?"
-	if [[ $REPLY =~ ^[Yy]$ ]]
+	if [[$REPLY =~ ^[Yy]$]]
 	then
-		chmod a+x $2
-		bash $2 -s
-		rm $2
-		if [[ $3 != '' ]]
-		then
-			apt $3
-		fi
-	else 
+	chmod a+x $2
+	bash $2 -s
+	rm $2
+	if [[$3 != '']]
+	then
+	apt $3
+	fi
+	else
 		echo "$2 not installed"
 	fi
 }
