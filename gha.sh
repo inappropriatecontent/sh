fd31dd53dcd16b0c2875364c8f32a4654ae0ca65
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-18 01:02:29 -0700

    update gha.sh

--- gha.sh
+++ gha.sh
@@ -1,13 +1,13 @@
 #! usr/bin/env bash
-gh auth login -s delete_repo 
-gh extension install yusukebe/gh-markdown-preview >/dev/null
-gh extension install korosuke613/gh-user-stars >/dev/null
+gh auth login -s delete_repo
+gh extension install yusukebe/gh-markdown-preview
+gh extension install korosuke613/gh-user-stars
 gh extension install gennaro-tedesco/gh-s
 gh alias set stars gh-user-stars
 gh alias set p markdown-preview
 if [type -p xplr >/dev/null]
-then 
-	gh extension install sayanarijit/gh-xplr
-	gh alias set x xplr
-	alias ghx='read -r -p "Clone Search?" | gh xplr "$(gh s $1)"'
+then
+gh extension install sayanarijit/gh-xplr
+gh alias set x xplr
+alias ghx = 'read -r -p "Clone Search?" | gh xplr "$(gh s $1)"'
 fi
