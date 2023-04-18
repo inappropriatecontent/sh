da379b78211edd470d8a1e44ffeefd08bb6188d3
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-17 20:47:46 -0700

    update ba.sh

--- ba.sh
+++ ba.sh
@@ -2,9 +2,10 @@
 cd sh
 chmod a+x fd.sh gh.sh gha.sh nvm.sh xplr.sh
 mod() {
-	read -p "Get $($1)?" -n 1 -ei Y r
-	if ["$r" = [Yy]*]; then
-	cat "$1.sh" | bash
+	2 = echo "$1" | sed 's/.sh$//'
+	read -p "Install?" -ei "$2" reply
+	if ["$reply" = "$2"]; then
+	echo "./$1"
 }
 
 cd ~
