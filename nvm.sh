562ee2ca8fb5bba2329dc93884aa4ef5fbd65d30
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-04-18 00:59:23 -0700

    update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -1,11 +1,11 @@
 #! usr/bin/env bash
 cd ~
-git clone https://github.com/nvm-sh/nvm .nvm >/dev/null
+git clone https://github.com/nvm-sh/nvm .nvm
 cd ~/.nvm
 git checkout v0.39.3
-. ./install.sh >/dev/null
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+. ./install.sh
+export NVM_DIR = "$HOME/.nvm"
+[-s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/nvm.sh"
+[-s "$NVM_DIR/bash_completion"] && \. "$NVM_DIR/bash_completion"
 nvm install 18
-npm add --global http-server hget shfmt
+npm add --global http-server hget
