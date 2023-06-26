fc6a2aeefd1abfd4219e509b12a16b4551f8565f
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-06-25 20:56:41 -0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,7 +1,11 @@
-#!/bin/bash 
 sudo apt-get update -y
 sudo apt-get full-upgrade -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash -
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt-get install gh -y
 curl -JL --output code.deb https://go.microsoft.com/fwlink/?LinkID=760868
 sudo apt install ./code.deb -y
 rm code.deb
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash -
+exit
