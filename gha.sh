512bb13d991cf60a2041efd15f4bb4a8e1e1677c
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-07-02 23:43:29 -0700

    Update gha.sh

--- gha.sh
+++ gha.sh
@@ -3,7 +3,7 @@ gh auth login -s delete_repo
 gh extension install yusukebe/gh-markdown-preview
 gh extension install korosuke613/gh-user-stars
 gh extension install gennaro-tedesco/gh-s
-gh alias set stars gh-user-stars
+gh alias set stars user-stars
 gh alias set p markdown-preview
 if [ "$(type -p xplr >/dev/null)" != "" ]; then
     gh extension install sayanarijit/gh-xplr
