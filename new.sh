f6086befd0be367a11c23d9d9239c4417111e264
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-08-14 07:18:04 -0700

    scripted

--- new.sh
+++ new.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo apt-get update -y
 sudo apt-get full-upgrade -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash -
@@ -9,4 +10,3 @@ curl -JL --output code.deb https://go.microsoft.com/fwlink/?LinkID=760868
 sudo apt install ./code.deb -y
 rm code.deb
 gh auth login -s
-exit
