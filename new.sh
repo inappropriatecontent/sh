ca26ed679191ce5eb5a2aba48fa690174876affc
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-08-14 23:21:58 -0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -7,8 +7,10 @@ sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt-get install gnome-keyring gh -y
 curl -JL --output code.deb https://go.microsoft.com/fwlink/?LinkID=760868
-sudo apt install ./code.deb -y
-rm code.deb
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install --lts
 npm install --global @gouch/to-title-case
+sudo apt install ./code.deb -y
+rm code.deb
 gh auth login -s
