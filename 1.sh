9ff122b796786548bc21637a220fae94091df5c9
Jmyle <97194551+inappropriatecontent@users.noreply.github.com>
2023-09-02 23:04:11 -0700

    dd

--- 1.sh
+++ 1.sh
@@ -13,12 +13,12 @@ nvm install 18 --latest-npm
 
 sudo apt-get upgrade -y
 
-type -p curl >/dev/null || sudo apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-sudo apt update \
-sudo apt install gh -y
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+	sudo apt update &&
+	sudo apt install gh -y
 
 gh extension install gennaro-tedesco/gh-s
 gh extension install yusukebe/gh-markdown-preview
